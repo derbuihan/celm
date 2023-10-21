80566f8e4e27bd2de93391de0619ebb48e51575d
derbuihan <derbuihan@gmail.com>
2023-10-22 02:13:11 +0900

    support multi declarations

--- src/InferType.elm
+++ src/InferType.elm
@@ -1,20 +1,10 @@
 module InferType exposing (inferType)
 
 import Elm.Syntax.File exposing (File)
-import Parser exposing (DeadEnd, Problem(..))
-import Typed.Declaration exposing (fromNodeDeclaration)
-import Typed.File exposing (TypedFile)
+import Parser exposing (DeadEnd)
+import Typed.File exposing (TypedFile, fromFile)
 
 
 inferType : File -> Result (List DeadEnd) TypedFile
-inferType file =
-    case file.declarations of
-        [] ->
-            Err [ DeadEnd 0 0 (Problem "Empty file") ]
-
-        decl :: _ ->
-            let
-                typedDeclaration =
-                    fromNodeDeclaration decl
-            in
-            Result.map (\d -> { declarations = [ d ] }) typedDeclaration
+inferType =
+    fromFile
--- src/Typed/File.elm
+++ src/Typed/File.elm
@@ -1,9 +1,61 @@
-module Typed.File exposing (TypedFile)
+module Typed.File exposing (TypedFile, fromFile)
 
-import Typed.Declaration exposing (TypedDeclaration)
+import Elm.Syntax.Declaration exposing (Declaration)
+import Elm.Syntax.File exposing (File)
+import Elm.Syntax.Node exposing (Node)
+import Parser exposing (DeadEnd)
+import Typed.Declaration exposing (TypedDeclaration, fromNodeDeclaration)
 import Typed.Node exposing (TypedNode)
 
 
 type alias TypedFile =
     { declarations : List (TypedNode TypedDeclaration)
     }
+
+
+fromFile : File -> Result (List DeadEnd) TypedFile
+fromFile file =
+    let
+        declarations : List (Node Declaration)
+        declarations =
+            file.declarations
+
+        declarationList : List (Result (List DeadEnd) (TypedNode TypedDeclaration))
+        declarationList =
+            declarations |> List.map fromNodeDeclaration
+
+        typedDeclarations : List (TypedNode TypedDeclaration)
+        typedDeclarations =
+            declarationList
+                |> List.filterMap
+                    (\x ->
+                        case x of
+                            Ok declaration ->
+                                Just declaration
+
+                            Err _ ->
+                                Nothing
+                    )
+
+        deadEndList : List DeadEnd
+        deadEndList =
+            declarationList
+                |> List.filterMap
+                    (\x ->
+                        case x of
+                            Ok _ ->
+                                Nothing
+
+                            Err err ->
+                                Just err
+                    )
+                |> List.concat
+    in
+    case deadEndList of
+        [] ->
+            Ok
+                { declarations = typedDeclarations
+                }
+
+        _ ->
+            Err deadEndList
